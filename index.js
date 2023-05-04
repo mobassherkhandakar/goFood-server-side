c488dd1e75bea4f6e29460e27575f32fa107d483
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-05-04 10:41:43 +0600

    server side deploy and setup all is done

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const express = require('express');
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
+const chefs = require("./data/chafeData.json");
+app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Chef Recipe Hunter Server is Running..............");
+});
 
-app.use(cors())
+app.get("/chafe", (req, res) => {
+  res.send(chefs);
+});
 
-app.get("/",(req,res)=>{
-  res.send('Chef Recipe Hunter Server is Running..............')
+app.get("/chafe/:id", (req,res)=>{
+  const id = parseInt(req.params.id);
+  const selectedID = chefs.find(cf=> parseInt(cf.id) === id)
+  res.send(selectedID)
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});
